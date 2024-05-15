b7f86d2a105d8c8d01452864929ac23d962afdee
appureddyt <apputmrreddy@gmail.com>
2024-05-15 17:35:05 +0100

    iusregfbues

--- ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
+++ ContactManager/obj/Debug/net8.0/ContactManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12313914bf4e9fea3c8bc8f475cde6308346294e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88b0c19a3027dfd5d911afbe23209ce2308eeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
